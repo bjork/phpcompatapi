64d40b54d7928306521ac3edc1d247d94543c410
Aki Björklund <aki@h1.fi>
2015-06-18 21:42:03 +0300

    minor indentation fix

--- app/app.php
+++ app/app.php
@@ -6,11 +6,9 @@
 	<link rel="stylesheet" href="<?php echo WCT_ROOT_PATH ?>assets/css/app.css">
 </head>
 <body>
-<div id="compatibility-tester"></div>
-<script src="<?php echo WCT_ROOT_PATH ?>assets/js/react.js"></script>
-<script>
-var wct_api_root = '<?php echo WCT_ROOT_PATH ?>';
-</script>
-<script src="<?php echo WCT_ROOT_PATH ?>assets/js/build/app.js"></script>
+	<div id="compatibility-tester"></div>
+	<script src="<?php echo WCT_ROOT_PATH ?>assets/js/react.js"></script>
+	<script>var wct_api_root = '<?php echo WCT_ROOT_PATH ?>';</script>
+	<script src="<?php echo WCT_ROOT_PATH ?>assets/js/build/app.js"></script>
 </body>
 </html>
