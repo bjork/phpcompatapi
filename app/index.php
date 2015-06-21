d67bf2172326289b38363459bf1e75e04a4995e3
Aki Björklund <aki@h1.fi>
2015-06-21 08:13:52 +0300

    Fix subfolder loading bug

--- app/index.php
+++ app/index.php
@@ -7,24 +7,25 @@ if ( file_exists( '../config.php' ) ) {
 	die( 'Missing config.php. See config-sample.php.' );
 }
 
-// Validate configuration
+// Validate configuration.
 if ( ! defined('WCT_ROOT_PATH') || ! defined('WCT_PHP_VERSION') ) {
 	die( 'Invalid configuration. See config-sample.php.' );
 }
 
-// Make sure root path begins and ends with a slash
+// Make sure root path begins and ends with a slash.
 if ( '/' !== substr( WCT_ROOT_PATH, 0, 1 )
 	|| '/' !== substr( WCT_ROOT_PATH, strlen( WCT_ROOT_PATH ) - 1 ) ) {
 	die( 'Invalid configuration. WCT_ROOT_PATH should begin and end with /.' );
 }
 
+// If not defined in configuration, define WCT_TEMP_DIR with the default.
 if ( ! defined( 'WCT_TEMP_DIR' ) ) {
 	define( WCT_TEMP_DIR, sys_get_temp_dir() );
 }
 
 // Main controller: run the API or the JS UI.
-if ( '/api/' === substr( $_SERVER['REQUEST_URI'], 0, 5 ) ) {
+if ( WCT_ROOT_PATH . 'api/' === substr( $_SERVER['REQUEST_URI'], 0, strlen( WCT_ROOT_PATH . 'api/' ) ) ) {
 	include 'api/index.php';
-} else {
+} else if ( WCT_ROOT_PATH === $_SERVER['REQUEST_URI'] ) {
 	include 'app.php';
 }
